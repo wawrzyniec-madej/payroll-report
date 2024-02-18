1ccebb0cfde6ed665ee3d1e0a001510bcf47fb3f
Wawrzyniec Madej <wawrzyniec.madej@gmail.com>
2024-02-18 09:46:54 +0100

    Ran more rules

--- src/php-cs-fixer.php
+++ src/php-cs-fixer.php
@@ -11,6 +11,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'declare_strict_types' => true,
+        'final_class' => true,
     ])
     ->setFinder($finder)
 ;
